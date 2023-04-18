e1236d7f03b7343616784f3f5bf9ddbea047528c
CoolPlayersAction52 <112393881+CoolPlayersAction52@users.noreply.github.com>
2023-04-18 23:33:22 +0900

    Header of API Added

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,8 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Pokedex API</h1>
+      <br></br>
       <form onSubmit = {handleSubmit}>
         <input type="text" 
         onChange={handleChange} 
